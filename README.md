837b85f3af572d9e53eb72a82e4c2e43e128a4d6
Ajay Solleti <solleti.ajay@gmail.com>
2017-12-03 00:29:06 +0530

    completed "Corruption Checksum" - Day 2 - Advent of Code 2017

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # adventofcode-2017
+
+* Day 1: Inverse Captcha `node days/1.js`
+* Day 2: Corruption Checksum `node days/2.js`
--- app.js
+++ app.js
@@ -3,6 +3,4 @@ const argv = require('yargs').argv
 if (!argv.day | !Number.isInteger(argv.day))
   console.error('usage: node app.js --day [number]')
 
-if (argv.day === 1) {
-  console.log('day 1 solution!')
-}
+console.log(`day ${argv.day} solution!`)
